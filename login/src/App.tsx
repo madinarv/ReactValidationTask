ddbab6b9eec7fd4fc035f9ed76414b402ee70380
madinarv <madinarv@code.edu.az>
2023-04-19 01:27:36 +0400

    react validation task

---
+++ login/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+import LoginPage from './pages/LoginPage';
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <LoginPage />
+    </div>
+  )
+}
+
+export default App
